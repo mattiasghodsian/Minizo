5d3d40ffffca3479d2a96697157a370021f29ae7
Rakma <mattiasghodsian@gmail.com>
2024-04-28 19:10:13 +0200

    feat: bump minizo version to latest

--- src-backend/routes/routes-backend.js
+++ src-backend/routes/routes-backend.js
@@ -30,7 +30,7 @@ backendRouter.get('/info', (req, res) => {
 
   res.send({
     'name': 'Minizo',
-    'version': '0.1.0',
+    'version': '0.1.2',
     'github': 'https://github.com/mattiasghodsian/Minizo',
     'environment': process.env.ENV,
     'musicStorage': MUSIC_STORAGE,
