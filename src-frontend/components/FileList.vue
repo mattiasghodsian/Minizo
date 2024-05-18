9e87cb990fd39f9d122291820ed172173f33d5fe
Rakma <mattiasghodsian@gmail.com>
2024-05-18 13:55:44 +0200

    fix:  mobile view, minify file list and remove now unused funcs

--- src-frontend/components/FileList.vue
+++ src-frontend/components/FileList.vue
@@ -7,18 +7,6 @@ import FileMetaData from '@/components/modals/FileMetaData.vue';
 const apiStore = useApiStore();
 const fileMetaModal = ref<boolean>(false);
 
-const getFileExtension = (fileName: string): string => {
-  const parts = fileName.split('.');
-  const extension = parts[parts.length - 1];
-  return extension.toLowerCase();
-}
-
-const splitFileName = (fileName: string): [] => {
-  const extensionIndex = fileName.lastIndexOf('.');
-  const nameWithoutExtension = fileName.substring(0, extensionIndex);
-  return nameWithoutExtension.split('-').map(part => part.trim()) as [];
-}
-
 const getMetaData = async (index: number): Promise<void|boolean> => {
   const files = apiStore.fileList as [];
   const file = files[index] ?? null;
@@ -42,13 +30,10 @@ const getMetaData = async (index: number): Promise<void|boolean> => {
   <section class="flex flex-col w-full max-h-[500px] md:max-h-[400px] overflow-x-auto">
     <div v-for="(file, index) in apiStore.fileList" :key="index"
       class="flex hover:bg-minizo-red hover:text-white text-sm md:text-md px-2 py-1 gap-2 just border-b border-minizo-red pt-2">
-      <span class="w-4/5 cursor-pointer select-none" @click="getMetaData(index)">
-        {{ splitFileName(file)[0] }} - {{ splitFileName(file)[1] }}
+      <span class="flex w-3/5 md:w-4/5 cursor-pointer select-none" @click="getMetaData(index)">
+        {{ file }}
       </span>
-      <span class="w-0.5/5">
-        {{ getFileExtension(file) }}
-      </span>
-      <span class="w-1/5">
+      <span class="flex w-2/5 md:w-1/5 items-center">
         <FileActions :index="index" :fileName="file" />
       </span>
     </div>
