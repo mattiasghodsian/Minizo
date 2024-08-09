f5701a6cb126e8ab5b0261d6c2793fb3609885ff
Rakma <mattiasghodsian@gmail.com>
2024-08-09 12:21:53 +0200

    fix: mobile view

--- src-frontend/views/DownloadView.vue
+++ src-frontend/views/DownloadView.vue
@@ -81,9 +81,9 @@ const download = async (): Promise<void> => {
     </div>
     <div class="flex gap-2 flex-col justify-center">
       <div class="bg-gray-700 p-1 rounded-full overflow-hidden relative z-10">
-        <InputText v-model="videoUrl" class="w-full px-8 h-16 rounded-full" unstyled
+        <InputText v-model="videoUrl" class="w-full px-8 h-10 md:h-16 rounded-full" unstyled
           placeholder="https://music.youtube.com/watch?v=xxxxxxxxxxx" />
-        <Button type="button" label="Download" severity="danger" iconPos="right" class="!absolute top-1 right-1 h-16 !rounded-full" :loading="loading" @click="download()"/>
+        <Button type="button" label="Download" severity="danger" iconPos="right" class="!absolute top-1 right-1 h-10 md:h-16 !rounded-full" :loading="loading" @click="download()"/>
       </div>
       <div class="flex flex-col md:flex-row gap-2 justify-between bg-gray-700 px-3 pt-4 pb-2 mx-10 text-sm -top-5 rounded-xl overflow-hidden relative">
         <div class="flex md:w-2/6 lg:w-full items-center justify-center md:justify-normal gap-2 text-white">
@@ -103,7 +103,7 @@ const download = async (): Promise<void> => {
   </section>
 
   <section class="text-xs w-full text-center text-gray-400">
-    <p class="px-20 py-2">
+    <p class="px-8 md:px-20 py-2">
       Downloading copyrighted content without authorization is illegal. This project is for educational purposes only. Ensure you have the right to download and use the content.
     </p>
   </section>
