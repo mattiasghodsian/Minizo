a92373c5d644bc7264a3f2213cdd7bdceeb3728a
Rakma <mattiasghodsian@gmail.com>
2024-08-10 13:08:11 +0200

    feat: add more global primevue components

--- src-frontend/main.ts
+++ src-frontend/main.ts
@@ -18,6 +18,8 @@ import Select from 'primevue/select';
 import Button from 'primevue/button';
 import Toast from 'primevue/toast';
 import Dialog from 'primevue/dialog';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 app.use(createPinia());
 app.use(router);
@@ -37,5 +39,7 @@ app.component('ToggleSwitch', ToggleSwitch);
 app.component('Select', Select);
 app.component('Button', Button);
 app.component('Toast', Toast);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
 
 app.mount('#app');
