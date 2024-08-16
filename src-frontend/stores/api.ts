6712b71ffde0837a53b436c3e8b70fead5b09e2d
Rakma <mattiasghodsian@gmail.com>
2024-08-16 15:33:50 +0200

    feat: remove fileMeteData key in store

--- src-frontend/stores/api.ts
+++ src-frontend/stores/api.ts
@@ -25,7 +25,6 @@ export const useApiStore = defineStore('api', {
     directories: [],
     currentViewDIrectory: '',
     fileList: {},
-    fileMetaData: []
   }),
   actions: {
     async getAbout() {
