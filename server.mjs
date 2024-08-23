2b7598b53cb69ff99a29f8ad0be21399483f8c9a
Rakma <mattiasghodsian@gmail.com>
2024-08-23 13:32:32 +0200

    feat: rework Auth to use bearer jwt token and new endpoint to get token

--- server.mjs
+++ server.mjs
@@ -1,16 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import basicAuth from 'express-basic-auth';
 import { backendRouter } from './src-backend/routes/routes-backend.js';
 import { frontendRouter } from './src-backend/routes/routes-frontend.js';
 import { hasEnvArgs } from './src-backend/helpers/bitwiseHelper.js';
+import { verifyToken } from './src-backend/controllers/authController.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-const usr = process.env.AUTH_USER;
-const pass = process.env.AUTH_PASS;
 
 // Middleware to check if environment arguments are present
 app.use((req, res, next) => {
@@ -23,23 +21,8 @@ app.use((req, res, next) => {
   next();
 });
 
-const apiAuthMiddleware = (req, res, next) => {
-  if (usr && pass && req.originalUrl !== "/api/info") {
-    basicAuth({
-      users: { [usr]: pass },
-      unauthorizedResponse: function getUnauthorizedResponse(req) {
-        return req.auth
-          ? { status: 401, message: 'Credentials rejected' }
-          : { status: 401, message: 'No credentials provided' };
-      }
-    })(req, res, next);
-  } else {
-    next();
-  }
-};
-
 app.use(express.json());
-app.use('/api', apiAuthMiddleware, backendRouter);
+app.use('/api', verifyToken, backendRouter);
 app.use('/', frontendRouter);
 
 app.listen(port, () => {
