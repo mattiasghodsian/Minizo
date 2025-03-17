829ec1f43ef10ce4a3a28f2bd721ef4d47c23ca1
Rakma <mattiasghodsian@gmail.com>
2025-03-17 22:38:15 +0100

    feat: smaller font size for nav

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -18,8 +18,8 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'flex items-center gap-4 text-xl text-white fill-white bg-gray-800 px-6 py-2 border-l-2 border-minizo-green rounded-tr-lg rounded-br-lg'
-        : 'flex items-center gap-4 text-xl text-gray-400 px-6 py-2 hover:bg-gray-800 border-l-2 border-transparent hover:text-white hover:border-l-2 hover:border-minizo-green hover:rounded-tr-lg hover:rounded-br-lg',
+        ? 'flex items-center gap-4 text-lg text-white fill-white bg-gray-800 px-6 py-2 border-l-2 border-minizo-green rounded-tr-lg rounded-br-lg'
+        : 'flex items-center gap-4 text-lg text-gray-400 px-6 py-2 hover:bg-gray-800 border-l-2 border-transparent hover:text-white hover:border-l-2 hover:border-minizo-green hover:rounded-tr-lg hover:rounded-br-lg',
 );
 </script>
 
